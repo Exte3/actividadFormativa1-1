9b10b05b30c6ff15710297718221a14db8c278fd
Marcelo López <marcelolopezhernandez@gmail.com>
2021-09-10 10:32:24 -0300

    Clase Medicamento

---
+++ src/com/actividad/Medicamento.java
@@ -0,0 +1,59 @@
+package com.actividad;
+
+public class Medicamento {
+	private String codigo;
+	private Double precioBase;
+	private String nombre;
+	private Boolean generico;
+	private String contraindicaciones;
+	
+	public Medicamento(String codigo, Double precioBase, String nombre, Boolean generico) {
+		super();
+		this.codigo = codigo;
+		this.precioBase = precioBase;
+		this.nombre = nombre;
+		this.generico = generico;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Double getPrecioBase() {
+		return precioBase;
+	}
+
+	public void setPrecioBase(Double precioBase) {
+		this.precioBase = precioBase;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Boolean getGenerico() {
+		return generico;
+	}
+
+	public void setGenerico(Boolean generico) {
+		this.generico = generico;
+	}
+
+	public String getContraindicaciones() {
+		return contraindicaciones;
+	}
+
+	public void setContraindicaciones(String contraindicaciones) {
+		this.contraindicaciones = contraindicaciones;
+	}
+	
+	
+}
