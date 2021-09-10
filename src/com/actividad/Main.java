3829f526a8b7e8c5c23121ecfa18bc2dc26d8ff8
Marcelo López <marcelolopezhernandez@gmail.com>
2021-09-10 11:24:58 -0300

    testing

--- src/com/actividad/Main.java
+++ src/com/actividad/Main.java
@@ -5,9 +5,8 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		Medicamento paracetamol = new Medicamento("101", 6000.0, "Paracetamol 1000mg", true);
+		Medicamento paracetamol = new Medicamento("101", 16000.0, "Paracetamol 1000mg", true);
 		Medicamento ibuprofeno = new Medicamento("102", 3000.0, "Ibuprofeno 500mg", false);
-		
 		Medicamento loratadina = new Medicamento("103", 500.0, "Loraradina 500mg", false);
 		
 		
@@ -17,17 +16,22 @@ public class Main {
 		registroProducto.agregarMedicamento(paracetamol);
 		registroProducto.agregarMedicamento(ibuprofeno);
 		registroProducto.agregarMedicamento(loratadina);
-		System.out.println(registroProducto.getMedicamentos());
 		
-		registroProducto.agregarMedicamento(ibuprofeno);
-		System.out.println(registroProducto.getMedicamentos());
+		registroProducto.agregarMedicamento(loratadina);
+		registroProducto.agregarMedicamento(loratadina);
 		
+		
+		registroProducto.listarTodosMedicamentos();
+		
+		registroProducto.recargarMedicamentos();
+		System.out.println("\nMedicamentos Recargo Aplicado \n");
+		
+		registroProducto.listarTodosMedicamentos();
+		
+		//SOLO PRODUCTOS > $10.000
+		System.out.println("\nMedicamentos > $10000\n");
 		registroProducto.listarMedicamentos();
-		registroProducto.listarSuplementos();
-		System.out.println("------");
-		registroProducto.eliminarMedicamentos();
-		System.out.println("------");
-		registroProducto.listarMedicamentos();
+
 		
 		
 		
--- src/com/actividad/RegistroProducto.java
+++ src/com/actividad/RegistroProducto.java
@@ -4,114 +4,119 @@ import java.util.ArrayList;
 
 public class RegistroProducto {
 
-	//1. Declarar y crear una Colección
+	// 1. Declarar y crear una Colección
 	private ArrayList<Medicamento> medicamentos;
 	private ArrayList<Suplemento> suplementos;
-	
-	//2. Agregar Producto (Medicamento o Suplemento Alimenticio), sólo si no existe el código.
-	
+
+	// 2. Agregar Producto (Medicamento o Suplemento Alimenticio), sólo si no existe
+	// el código.
+
 	public RegistroProducto() {
 		super();
 		ArrayList<Medicamento> medicamentos = new ArrayList<Medicamento>();
 		ArrayList<Suplemento> suplementos = new ArrayList<Suplemento>();
-		
+
 		this.medicamentos = medicamentos;
 		this.suplementos = suplementos;
 	}
-	
-	
+
 	public void agregarMedicamento(Medicamento medicamento) {
-		//Buscar si existe
+		// Buscar si existe
 		boolean existe = false;
 		int index = 0;
-		
-		while(index < this.medicamentos.size() && !existe) {
-			//Encuentro el medicamento
-			if(this.medicamentos.get(index).getCodigo().equals(medicamento.getCodigo()) ) {
+
+		while (index < this.medicamentos.size() && !existe) {
+			// Encuentro el medicamento
+			if (this.medicamentos.get(index).getCodigo().equals(medicamento.getCodigo())) {
 				existe = true;
 			}
 			index++;
-		
+
 		}
-		if(!existe) {
+		if (!existe) {
 			this.medicamentos.add(medicamento);
 		}
-		
-	}
-	
 
+	}
 
 	public void agregarSuplemento(Suplemento suplemento) {
-		
+
 		boolean existe = false;
 		int index = 0;
-		
-		while(index < this.suplementos.size() && !existe) {
-			//Encuentro el suplemento
-			if(this.suplementos.get(index).getCodigo().equals(suplemento.getCodigo()) ) {
+
+		while (index < this.suplementos.size() && !existe) {
+			// Encuentro el suplemento
+			if (this.suplementos.get(index).getCodigo().equals(suplemento.getCodigo())) {
 				existe = true;
 			}
 			index++;
-		
+
 		}
-		if(!existe) {
+		if (!existe) {
 			this.suplementos.add(suplemento);
 		}
-		
+
 	}
 
-
-
-	
-	//3. Listar todos los Productos(Medicamento o Suplemento Alimenticio) que tengan un precio base superior a los $10000.
+	// 3. Listar todos los Productos(Medicamento o Suplemento Alimenticio) que
+	// tengan un precio base superior a los $10000.
 	public void listarMedicamentos() {
-		//Precio base > 10000
-		for (int i=0;i<this.medicamentos.size();i++) {
-			if(this.medicamentos.get(i).getPrecioBase() > 10000) {
+		// Precio base > 10000
+		for (int i = 0; i < this.medicamentos.size(); i++) {
+			if (this.medicamentos.get(i).getPrecioBase() > 10000) {
 				System.out.println(this.medicamentos.get(i).toString());
 			}
 		}
 	}
-	
+
+	public void listarTodosMedicamentos() {
+		for (int i = 0; i < this.medicamentos.size(); i++) {
+			System.out.println(this.medicamentos.get(i).toString());
+		}
+	}
+
 	public void listarSuplementos() {
-		//Precio base > 10000
-		for (int i=0;i<this.suplementos.size();i++) {
-			if(this.suplementos.get(i).getPrecioBase() > 10000) {
+		// Precio base > 10000
+		for (int i = 0; i < this.suplementos.size(); i++) {
+			if (this.suplementos.get(i).getPrecioBase() > 10000) {
 				System.out.println(this.suplementos.get(i).toString());
 			}
 		}
 	}
-	
-	
-	//4. Eliminar Producto: Eliminará todos los Productos cuyo precio base es menor a $2.000, indicando cuántos fueron
-	
+
+	// 4. Eliminar Producto: Eliminará todos los Productos cuyo precio base es menor
+	// a $2.000, indicando cuántos fueron
+
 	public void eliminarMedicamentos() {
-		this.medicamentos.removeIf( e -> e.getPrecioBase() < 2000);	
+		this.medicamentos.removeIf(e -> e.getPrecioBase() < 2000);
 	}
 
 	public void eliminarSuplementos() {
-		this.medicamentos.removeIf( e -> e.getPrecioBase() < 2000);	
+		this.medicamentos.removeIf(e -> e.getPrecioBase() < 2000);
 	}
-	
-	
+
 	public ArrayList<Medicamento> getMedicamentos() {
 		return medicamentos;
 	}
-
 
 	public void setMedicamentos(ArrayList<Medicamento> medicamentos) {
 		this.medicamentos = medicamentos;
 	}
 
-
 	public ArrayList<Suplemento> getSuplementos() {
 		return suplementos;
 	}
 
-
 	public void setSuplementos(ArrayList<Suplemento> suplementos) {
 		this.suplementos = suplementos;
 	}
-	
-	
+
+	public void recargarMedicamentos() {
+		for (int i = 0; i < this.medicamentos.size(); i++) {
+			if (!this.medicamentos.get(i).getGenerico()) {
+				this.medicamentos.get(i).setPrecioBase(this.medicamentos.get(i).getPrecioBase() * 1.2);
+			}
+		}
+	}
+
 }
